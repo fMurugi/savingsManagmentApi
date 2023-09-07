43b81033ac2dd26a75a964afab7d5482621c5efb
fMurugi <fionamurugi99@gmail.com>
2023-09-07 12:03:30 +0300

    initial commit

---
+++ src/main/java/com/fiona/savingsManagementApi/SavingsManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiona.savingsManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiona/savingsManagementApi/SavingsManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiona.savingsManagementApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
