a8038ea6d74f64ec81e0c770d2d6c2e8f560ed73
fMurugi <fionamurugi99@gmail.com>
2023-09-08 16:11:11 +0300

    add excpetion handlers

--- src/main/java/com/fiona/savingsManagementApi/Exceptions/ExceptionResponse.java
+++ src/main/java/com/fiona/savingsManagementApi/Exceptions/ExceptionResponse.java
@@ -1,15 +1,22 @@
 package com.fiona.savingsManagementApi.Exceptions;
 
-import lombok.AllArgsConstructor;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+@Builder
+@Getter
+@Setter
 public class ExceptionResponse {
     private String message;
     private HttpStatus status;
     private LocalDateTime timeStamp;
 
     public ExceptionResponse(String message, HttpStatus httpStatus, LocalDateTime now) {
+        this.message = message;
+        this.status = httpStatus;
+        this.timeStamp = now;
     }
+
 }
--- src/main/java/com/fiona/savingsManagementApi/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fiona/savingsManagementApi/Exceptions/GlobalExceptionHandler.java
@@ -2,17 +2,41 @@ package com.fiona.savingsManagementApi.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
-public class GlobalExceptions {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request){
+    public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException ex){
         return  new ResponseEntity<>(new ExceptionResponse(ex.getMessage(), HttpStatus.NOT_FOUND, LocalDateTime.now()),HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleValidationExceptions(MethodArgumentNotValidException exception){
+        Map<String,String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName,errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handlePathNotFoundException(NoHandlerFoundException ex) {
+        String errorMessage = "Path not found: " + ex.getRequestURL();
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 }
