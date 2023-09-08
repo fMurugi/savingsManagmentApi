4e493191f022f4566d406558250ca311d6d719cc
fMurugi <fionamurugi99@gmail.com>
2023-09-08 19:29:46 +0300

    write unit tests for create customer and get all customers

--- src/test/java/com/fiona/savingsManagementApi/CustomerTest.java
+++ src/test/java/com/fiona/savingsManagementApi/CustomerTest.java
@@ -1,6 +1,10 @@
 package com.fiona.savingsManagementApi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiona.savingsManagementApi.Customer.Model.CustomerModel;
+import com.fiona.savingsManagementApi.Customer.Payload.CustomerPayload;
 import com.fiona.savingsManagementApi.Customer.repository.CustomerRepository;
 import com.fiona.savingsManagementApi.Customer.service.CustomerService;
 import com.fiona.savingsManagementApi.SavingsProduct.model.SavingsProductModel;
@@ -8,13 +12,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -23,6 +30,11 @@ public class CustomerTest {
     private CustomerRepository customerRepository;
     @Autowired
     private CustomerService customerService;
+//    @Autowired
+//    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void testGetAllCustomers(){
@@ -51,5 +63,45 @@ public class CustomerTest {
         assertEquals(expectedCustomers,actualCustomers);
     }
 
-    
+    @Test
+    public void createCustomerTest() throws Exception {
+        CustomerPayload customer1 = new CustomerPayload();
+        customer1.setFirstName("cathy");
+        customer1.setLastName("cook");
+        customer1.setNationalId(23456780);
+        customer1.setPhoneNumber("0765097810");
+        customer1.setEmail("cathyCook@gmail.com");
+
+
+        customer1.setSavingsProducts(Collections.emptyList());
+
+        CustomerModel customerModel = new CustomerModel();
+        UUID expectedCustomerId = UUID.randomUUID();
+        customerModel.setCustomerId(expectedCustomerId);
+        customerModel.setFirstName("cathy");
+        customerModel.setLastName("cook");
+        customerModel.setNationalId(23456780);
+        customerModel.setPhoneNumber("0765097810");
+        customerModel.setEmail("cathyCook@gmail.com");
+        customerModel.setMemberNumber("M06520");
+        customerModel.setSavingsProducts(Collections.emptyList()); // Set an empty list of UUIDs
+
+        when(customerRepository.save(any())).thenReturn(customerModel);
+
+        String jsonResponse = customerService.createCustomer(customer1);
+
+        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+
+//        assertEquals(expectedCustomerId, jsonNode.get("customerId").asText());
+        assertEquals("cathy", jsonNode.get("firstName").asText());
+        assertEquals("cook", jsonNode.get("lastName").asText());
+        assertEquals(23456780, jsonNode.get("nationalId").asInt());
+        assertEquals("0765097810", jsonNode.get("phoneNumber").asText());
+        assertEquals("cathyCook@gmail.com", jsonNode.get("email").asText());
+        assertEquals("M06520", jsonNode.get("memberNumber").asText());
+        assertTrue(jsonNode.get("savingsProducts").isArray());
+        assertEquals(0, jsonNode.get("savingsProducts").size());
+    }
+
+
 }
